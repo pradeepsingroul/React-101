9e438dab474be000aae669312aa3480a67fdd5a8
predeep kumar singroul <prasingroul4899@gmail.com>
2023-08-11 00:20:06 +0530

    react_101_day_3

--- Day_3/myapp2/src/App.js
+++ Day_3/myapp2/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
+// react.useState;
 function App() {
+  const [count,setCount] = React.useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Count : {count}</h1>
+     <button onClick={()=> setCount(count + 1)}>INC</button>
+     <button onClick={()=> setCount(count - 1)}>DEC</button>
+     <button onClick={()=> setCount(0)}>RESET</button>
     </div>
   );
 }
