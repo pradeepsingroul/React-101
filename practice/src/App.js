ea5a2442711053c62fdbbd24cabbd86e5675a83f
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-18 07:24:12 +0530

    Practice

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ApiFetch from './Components/ApiFetchingHooks';
+import Counter from './Components/Counter';
+import Task from './Components/Task';
 
 function App() {
   return (
     <div className="App">
-      <ApiFetch/>
+      {/* <ApiFetch/> */}
+    {/* <Counter/> */}
+    <Task/>
     </div>
   );
 }
