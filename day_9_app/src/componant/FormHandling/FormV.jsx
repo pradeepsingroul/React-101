f9e16002742bad4ea2f8852f6001f07bd976f14d
predeep kumar singroul <prasingroul4899@gmail.com>
2023-12-20 09:11:54 +0530

    Form handling

--- day_9_app/src/componant/FormHandling/FormV.jsx
+++ day_9_app/src/componant/FormHandling/FormV.jsx
@@ -3,28 +3,34 @@ import React from "react";
 
 
 export default function FormV() {
-    const schema = {name: "", password:""}//we can add more fields
-    const [formData, setFormData] = React.useState(schema);
+    // const schema = { name: "", password: "", valid: false}//we can add more fields
+    const [formData, setFormData] = React.useState({});
 
 
     const HandleSubmit = (e) => {
         e.preventDefault()
         console.log(formData)
     }
-    const HandleChange = (e)=>{
-        console.log(e.target.value)
-        const {name,value} = e.target
-        console.log('value', value);
-        console.log('name', name);
-        setFormData({...formData,[name]:value})
+    const HandleChange = (e) => {
+        // console.log(e.target.value)
+        const { name, value,type, checked } = e.target
+        // console.log('checked', checked);
+        const val = type === "checkbox" ? checked : value
+        setFormData({ ...formData, [name]: val })
     }
 
 
     return <div>
 
         <form action="submit" onSubmit={HandleSubmit}>
-            <input type="text" name="name" value={formData.name} onChange={HandleChange}/>
-            <input type="text" name="password" value={formData.password} onChange={HandleChange}/>
+            <input type="text" name="name" value={formData.name} onChange={HandleChange} />
+            <input type="text" name="password" value={formData.password} onChange={HandleChange} />
+            <input type="checkbox" name="valid" value={formData.valid} onChange={HandleChange} />
+            <select name="country" value={formData.country} onChange={HandleChange}>
+                <option value="IN">india</option>
+                <option value="China">china</option>
+                <option value="US">us</option>
+            </select>
             <input type="submit" value="Submit" />
         </form>
     </div>
