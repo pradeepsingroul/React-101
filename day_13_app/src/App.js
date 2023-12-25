2be002467b7ef998d7d343c2056b6693fb6d4941
predeep kumar singroul <prasingroul4899@gmail.com>
2023-12-25 10:19:24 +0530

    Day_13

---
+++ day_13_app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterUsingReducer from './Componant/CounterUsingReducer';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterUsingReducer/>
+    </div>
+  );
+}
+
+export default App;
